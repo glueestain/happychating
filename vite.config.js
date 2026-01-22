4f277abc6f1c37982117a8a6837c2e259d6cbf41
glueestain <yakimlyuf@yandex.ru>
2026-01-22 13:50:34 +0300

    configure github pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+
+
 export default defineConfig({
+  base: "/https://github.com/glueestain/happychating/",
   plugins: [
     react(),
     VitePWA({
